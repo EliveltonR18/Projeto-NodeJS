89eb727bb40dd09b9feb2bf244bcbaf136d42b7c
Yuri França <yuridene50@gmail.com>
2020-09-04 21:12:17 -0300

    envio do projeto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -22,4 +22,63 @@ exports.Insert = (req, res, next) => {
     .catch(error => next(error));
 };
 
+exports.Drop = (req, res, next) => {
+    const nome = req.body.nome;
+    const valor = req.body.valor;
+
+    Produto.create({
+        nome: nome,
+        valor: valor
+    })
+
+    .then(produto => {
+        if (produto) {
+            res.status(status.OK).send(produto);
+        } else {
+            res.status(status.NOT_FOUND).send();
+        }
+    })
+
+    .catch(error => next(error));
+};
+exports.Select = (req, res, next) => {
+    const nome = req.body.nome;
+    const valor = req.body.valor;
+
+    Produto.create({
+        nome: nome,
+        valor: valor
+    })
+
+    .then(produto => {
+        if (produto) {
+            res.status(status.OK).send(produto);
+        } else {
+            res.status(status.NOT_FOUND).send();
+        }
+    })
+
+    .catch(error => next(error));
+};
+
+exports.Alter = (req, res, next) => {
+    const nome = req.body.nome;
+    const valor = req.body.valor;
+
+    Produto.create({
+        nome: nome,
+        valor: valor
+    })
+
+    .then(produto => {
+        if (produto) {
+            res.status(status.OK).send(produto);
+        } else {
+            res.status(status.NOT_FOUND).send();
+        }
+    })
+
+    .catch(error => next(error));
+};
+
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const ProductController = require ('../controllers/productController.js');
 const router = express.Router();
 
-router.post('/produtos', ProductController.Insert);
+router.post('/inserir', ProductController.Insert);
+router.post('/alterar', ProductController.Alter);
+router.post('/excluir', ProductController.Drop);
+router.post('/selecionar', ProductController.Select);
  
 module.exports = router;
